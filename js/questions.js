7a199d546846b13a5ac674cb5a577afdb7984b8f
Kim-Haul <bokugawa@naver.com>
2022-12-23 19:07:53 +0900

    :lipstick: 메인 화면 및 전체적인 UI 초안 퍼블리싱(1)

---
+++ js/questions.js
@@ -0,0 +1,10 @@
+const progressValueEl = document.querySelector('.progress .value');
+const numberEl = document.querySelector('.number');
+const questionEl = document.querySelector('.question');
+const choice1El = document.querySelector('.choice1');
+const choice2El = document.querySelector('.choice2');
+
+let currentNumber = 0;
+let mbti = '';
+
+function renderQuestion() {}
